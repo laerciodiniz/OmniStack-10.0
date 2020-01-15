0f80bcdb4a16fc0cde36d49c1128ef918045e1b4
Laercio Diniz <laerciodiniz@gmail.com>
2020-01-15 00:37:38 -0300

    criado backend configurado ambiente, pastas e rota POST...

---
+++ backend/src/config/security.js
@@ -0,0 +1,4 @@
+const DATABASE_URL =
+  "mongodb+srv://admin:admin@cluster0-5e5ec.mongodb.net/test?retryWrites=true&w=majority";
+
+module.exports = { DATABASE_URL };
---
+++ backend/src/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
+const app = express();
+const { DATABASE_URL } = require("./config/security");
+
+mongoose.connect(DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+app.use(express.json());
+app.use(routes);
+/*
+Métodos HTTP: GET, POST, PUT, DELETE
+
+Tipos de Parâmetros:
+- Query Params: request.query (Filtros, ordenacao, paginacao...)
+- Route Params: request.params (identificar um recurso na alteracao ou remocao)
+- Body: request.body (dados para criacao ou alteracao de um registro)
+*/
+
+app.listen(3333);
