a2da28f061a2a3c406bcb9021ac5ecabb5330bff
Laercio Diniz <laerciodiniz@gmail.com>
2020-01-16 23:03:55 -0300

    backend finalizado

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,7 +1,22 @@
 const axios = require("axios");
 const Dev = require("../models/Dev");
+const parseStringAsArray = require("../utils/parseStringAsArray");
+
+/*
+Index (Buscar uma lista)
+Show (exibir um unico dev)
+store (criar novo dev)
+update (alterar)
+destroy (deletar)
+*/
 
 module.exports = {
+  async index(request, response) {
+    const devs = await Dev.find();
+
+    return response.json(devs);
+  },
+
   async store(request, response) {
     const { github_username, techs, latitude, longitude } = request.body;
 
@@ -14,7 +29,7 @@ module.exports = {
 
       const { name = login, avatar_url, bio } = apiResponse.data;
 
-      const techsArray = techs.split(",").map(tech => tech.trim());
+      const techsArray = parseStringAsArray(techs);
 
       const location = {
         type: "Point",
