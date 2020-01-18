39333656c2d3a2e9d77d8cb865fa97e44913e0ba
Laercio Diniz <laerciodiniz@gmail.com>
2020-01-18 00:53:11 -0300

    frontend finalizado, componentizacao, styles...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./global.css";
 import "./App.css";
 import "./Sidebar.css";
+import "./Main.css";
+
+import DevItem from "./components/Devitem";
+import DevForm from "./components/DevForm";
 
 //Componente: bloco isolado de html, css e js, o qual não interfere no restante da aplicacao
 //Propriedade: Informacoes que um componente pai passa par ao componente filho
 //Estado: Informacoes mantidas pelo componente (lembrar: imutabilidade)
 
 function App() {
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get("/devs");
+
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(data) {
+    //e.preventDefault(); Evita que o comportamento padrão no submit do form vá para uma outra pagina
+
+    const response = await api.post("/devs", data);
+
+    setDevs([...devs, response.data]);
+  }
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required />
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required />
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required />
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
-      <main></main>
+
+      <main>
+        <ul>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
+        </ul>
+      </main>
     </div>
   );
 }
